d37c24ba8b9830bb7a46347149c8fb99b178bf55
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-01 05:17:01 -0400

    Completed foundations/09_sumAll

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(string) {
     reverse = ""
 
-    for (i = string.length - 1; i >= 0; i--) {
+    for (let i = string.length - 1; i >= 0; i--) {
         reverse += string[i];
     }
 
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (valA, valB) {
+  if (
+    valA < 0 ||
+    valB < 0 ||
+    valA % 1 !== 0 ||
+    valB % 1 !== 0 ||
+    typeof valA !== "number" ||
+    typeof valB !== "number"
+  ) {
+    return "ERROR";
+  }
 
+  const startValue = Math.min(valA, valB);
+  const endValue = Math.max(valA, valB);
+  let sum = 0;
+
+  for (let i = startValue; i <= endValue; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
