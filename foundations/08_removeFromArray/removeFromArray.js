f2f3d8c8e45c27970278040aff84a4c8f2ee3ba8
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-01 05:07:06 -0400

    Completed foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    newArray = array;
+
+    // Needs to remove all instances of that value from array
+    // Needs to be able to adapt to the amount of arguments given
+    // Needs to ignore any value that's not present from arguments
+    // Needs to work with strings too
+
+    for (const value of values) {
+        tempArray = newArray.filter(item => item !== value);
+        newArray = tempArray;
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
