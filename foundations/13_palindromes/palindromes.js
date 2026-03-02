6e0b8bfd13e74887eb7b1ad10a58e09092104ab6
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-02 08:14:45 -0400

    Completed foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let newString = string.replace(/\W/g, "").toLowerCase();
+  let startIndex = 0;
+  let endIndex = newString.length - 1;
 
+  while (startIndex < endIndex) {
+    if (newString[startIndex] !== newString[endIndex]) {
+      return false;
+    }
+
+    startIndex++;
+    endIndex--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
