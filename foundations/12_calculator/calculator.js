664fb29bcafc96ae0f2a9cba2e7c4f500eddf3b7
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-02 08:05:35 -0400

    Completed foundations/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
 
+  let total = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    total *= array[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  }
+
+  let total = a;
+
+  for (let i = total - 1; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +55,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
