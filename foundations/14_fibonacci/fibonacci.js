5af5d0179b71e11aa20524c0575590d6abf4d794
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-02 08:23:29 -0400

    Completed foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (val) {
+  let number = Number(val);
 
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  if (number === 0) {
+    return 0;
+  }
+
+  if (number < 3) {
+    return 1;
+  }
+
+  let a = 1;
+  let b = 1;
+
+  for (let i = 3; i <= number; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 };
 
 // Do not edit below this line
