5242dbfc64def12e0f665c7455ab871ae4f7b36c
AlexPBouds <alexcjboudreau@gmail.com>
2026-03-02 08:47:53 -0400

    Completed foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let oldest = array[0];
+  let oldestAge = array[0].yearOfDeath
+    ? array[0].yearOfDeath - array[0].yearOfBirth
+    : new Date().getFullYear() - array[0].yearOfBirth;
 
+  for (let i = 1; i < array.length; i++) {
+    currentAge = array[i].yearOfDeath
+      ? array[i].yearOfDeath - array[i].yearOfBirth
+      : new Date().getFullYear() - array[i].yearOfBirth;
+
+    if (currentAge > oldestAge) {
+      oldest = array[i];
+      oldestAge = currentAge;
+    }
+  }
+
+  return oldest;
 };
 
 // Do not edit below this line
